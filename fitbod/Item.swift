2e4d962b5b4c52b558dac490c7c100d077ba342f
Chris Saechao <chris_saechao@live.com>
2026-05-10 20:57:55 -0700

    Initial Commit

---
+++ fitbod/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  fitbod
+//
+//  Created by Chris Saechao on 5/10/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ fitbodTests/fitbodTests.swift
@@ -0,0 +1,18 @@
+//
+//  fitbodTests.swift
+//  fitbodTests
+//
+//  Created by Chris Saechao on 5/10/26.
+//
+
+import Testing
+
+struct fitbodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
